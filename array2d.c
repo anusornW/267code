d21ccac02b973f6d0ad9ca35b94a2a20193a4e5e
anusornW <78833104+anusornW@users.noreply.github.com>
2024-12-26 18:30:52 +0700

    add case2 of T[][]

--- array2d.c
+++ array2d.c
@@ -12,15 +12,36 @@ int main() {
 	
 	int i,j;
 	//input to C
-	/*
+	
 	for(i=0;i<4;i++) {
 		for(j=0;j<2;j++) {
 			printf("C[%d][%d]: ",i,j);
 			scanf("%d",&C[i][j]);
 		}
 	}
-	*/
+	
 	//input to T
+	//case1 loop
+	for(i=1;i<=6;i++) {
+		T[0][i]=5;
+		T[1][i]=0;
+		T[2][i]=5;
+		T[3][i]=0;
+		T[4][i]=5;
+	}
+	//case2 check row col
+	for(i=1;i<=5;i++) {
+		for(j=1;j<=6;i++) {
+			if () {
+				T[i][j]=5;
+			}
+			if () {
+				T[i][j]=0;
+			}
+		}
+	}
+	//case3 input from keyboard
+	
 	
 	//output from T
 	for(i=0;i<5;i++) {
