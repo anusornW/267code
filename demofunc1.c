9809bbdcf935e75a6f26a0713e5b0b62f7f8206f
anusornW <78833104+anusornW@users.noreply.github.com>
2025-01-14 14:42:11 +0700

    funcionts prototyping example

---
+++ demofunc1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+//prototype
+float InputDouble (void);
+float SumDouble ( float , float  );
+void PrintOut ( float );
+
+int main ( ) {
+	float a1, a2, sumVal;
+	a1 = InputDouble( );
+	fflush(stdin);
+	a2 = InputDouble( );
+	sumVal = SumDouble ( a1, a2 );
+	PrintOut ( sumVal );
+	return 0;
+}
+
+float InputDouble ( ) {
+	float x;
+	printf ( "\nInput real value : " );
+	scanf ( "%f", &x );
+	return x ;
+}
+float SumDouble ( float x, float y ) {
+	return x + y ;
+}
+void PrintOut ( float x ) {
+	printf ( "\n Result of sum is : %.2f", x );
+}
